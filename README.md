cb515f418d9afa5f69d3fee2d505678aa2d166d8
Fadi Buni <fadibuni1@gmail.com>
2024-01-12 12:37:08 -0800

    First commit of full project

---
+++ README.md
@@ -0,0 +1,6 @@
+This website is designed to offer a rando, two parts joke to the user using JokeAPI
+
+to run this website, you need to install the following packages:
+npm i  and then nodemon index.js
+
+That should be it :) Have fun
---
+++ index.js
@@ -0,0 +1,41 @@
+// HINTS:
+// 1. Import express and axios
+import express from "express";
+import axios from "axios";
+// 2. Create an express app and set the port number.
+const app = express();
+const port = 3000;
+const API_URL = "https://v2.jokeapi.dev/joke/Any?type=twopart";
+
+app.use(express.static("public"));
+
+
+app.get("/", async (req, res) => {
+    try {
+    const result = await axios.get(API_URL);
+    console.log(result.data);
+    res.render("index.ejs", { setup: result.data.setup, delivery: result.data.delivery });
+    } catch (error) {
+        console.log("An error happened: " + error.response.data);
+        res.status(500)
+      }
+});
+
+
+app.get("/get-joke", async (req, res) => {
+    try {
+    const result = await axios.get(API_URL);
+    console.log(result.data);
+    res.render("index.ejs", { setup: result.data.setup, delivery: result.data.delivery });
+    } catch (error) {
+        console.log("An error happened: " + error.response.data);
+        res.status(500)
+      }
+});
+
+
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+  });
+  
